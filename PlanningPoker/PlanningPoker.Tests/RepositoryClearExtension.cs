2211bde46c35a6c48c13bb0415ccf08cc11eff1f
Golovanov A.V <Golovanov_AV@directum.ru>
2020-08-20 08:00:01 +0400

    В DTO добавлены сеттеры

---
+++ PlanningPoker/PlanningPoker.Tests/RepositoryClearExtension.cs
@@ -0,0 +1,21 @@
+﻿using PlanningPoker.Data.Models;
+using PlanningPoker.Data.Repositories;
+using System.Linq;
+
+namespace PlanningPoker.Tests
+{
+  public static class RepositoryClearExtension
+  {
+    public static void Clear<T>(this IRepository<T> repository) where T : IEntity
+    {
+      var allEntities = repository.GetAll().ToList();
+
+      var amount = allEntities.Count;
+
+      for (var i = 0; i < amount; i++)
+      {
+        repository.Remove(allEntities[0].Id);
+      }
+    }
+  }
+}
