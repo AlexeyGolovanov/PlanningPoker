99a6246b9d6b807ccb6b3a5dbb81f7b913e54a76
Golovanov A.V <Golovanov_AV@directum.ru>
2020-08-20 07:26:34 +0400

    TimerService изменено название словаря с таймерами и словарь больше не статический

---
+++ PlanningPoker/PlanningPoker.Data/Models/Vote.cs
@@ -0,0 +1,30 @@
+﻿#nullable enable
+namespace PlanningPoker.Data.Models
+{
+  /// <summary>
+  /// Класс описывающий выбор пользователя
+  /// </summary>
+  public class Vote
+  {
+    /// <summary>
+    /// Конструктор класса
+    /// </summary>
+    /// <param name="user">Голосовавший пользователь</param>
+    /// <param name="card">Выбранная карта</param>
+    public Vote(User user, Card? card)
+    {
+      this.User = user;
+      this.Card = card;
+    }
+
+    /// <summary>
+    /// Голосовавший пользователь
+    /// </summary>
+    public User User { get; }
+
+    /// <summary>
+    /// Выбранная карта
+    /// </summary>
+    public Card? Card { get; set; }
+  }
+}
